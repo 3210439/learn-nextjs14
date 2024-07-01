1adf4e21280d513284a69cfce3ebb5a548075448
kimJongWan <kimdeiz@naver.com>
2024-07-01 23:09:22 +0900

    make data fetch with react normal function

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,20 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
 
-export default function tomato() {
-  return <h1>hello</h1>;
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <h1>{isLoading ? "Loading..." : JSON.stringify(movies)}</h1>;
 }
